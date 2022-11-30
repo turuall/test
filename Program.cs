6c843dc1603049ae0cc19ddf19ec8353580c2ffb
Turuall <turusin@mail.ru>
2022-11-30 12:27:41 +0800

    шаг 3. Код

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Input_string(int a)
+{
+    string[] result = new string[a];
+    for(int i = 0; i<a; i++)
+    {
+        Console.Write($"Введите {i} элемент: ");
+        result[i]= Console.ReadLine();
+    }
+    return result;
+}
+
+int Input_razmer()
+{
+    Console.WriteLine("Введите кол-во элементов массива: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string[] calculation(string[] array)
+{
+    int a = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+            a++;                       
+        }
+    }
+    string[] result = new string[a];
+    a = -1;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= 3)
+        {
+           a++;
+           result[a]= array[i];                       
+        }
+    }
+    return result;
+}
+
+void Print(string[] array)
+{
+    Console.WriteLine(string.Join(' ', array));
+}
+
+Print(calculation(Input_string(Input_razmer())));
